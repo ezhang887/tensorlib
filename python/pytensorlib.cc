edd376bc1d4987f1399783a66969a0b810b6fc29
Eric Zhang <ezhang887@gmail.com>
2022-03-16 21:56:47 -0700

    Add some more python bindings

--- python/pytensorlib.cc
+++ python/pytensorlib.cc
@@ -1,9 +1,24 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
+#include "core/dtypes.h"
 #include "core/tensor.h"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(pytensorlib, m) {
-    py::class_<Tensor>(m, "Tensor");
+  py::enum_<Dtype>(m, "Dtype")
+      .value("int8", Dtype::INT8)
+      .value("int16", Dtype::INT16)
+      .value("int32", Dtype::INT32)
+      .value("int64", Dtype::INT64)
+      .value("float", Dtype::FLOAT)
+      .value("double", Dtype::DOUBLE)
+      .export_values();
+
+  py::class_<Tensor>(m, "Tensor")
+      .def(py::init<std::vector<size_t>, Dtype, bool>())
+      .def("is_contiguous", &Tensor::is_contiguous)
+      .def("strides", &Tensor::strides)
+      .def("dims", &Tensor::dims);
 }
